2b3b7e4828f55104a31c3e97719d58eab10a0869
kevinmhadi <kevinmhadi@gmail.com>
2020-09-06 10:53:51 -0400

    added

--- .emacs.d/lisp/default.el
+++ .emacs.d/lisp/default.el
@@ -1,5 +1,3 @@
-(load "~/software/ess-16.04/lisp/ess-site")
-
 (load-file "~/.emacs.d/lisp/essh.el")
 (load-file "~/.emacs.d/lisp/emacs.func.el")
 ;; (load-file "~/blog.lisp")
@@ -8,6 +6,16 @@
 (global-set-key (kbd "C-c f") `ess-function-name)
 (global-set-key (kbd "C-c o") `ess-swv-knit)
 
+(add-hook 'sh-mode-hook 'highlight-indent-guides-mode)
+
+
+(add-hook 'inferior-ess-mode-hook
+	  (lambda ()
+	    (setq eldoc-mode nil)
+	    (setq ess-use-eldoc nil)))
+
+
+(load "~/software/ess-16.04/lisp/ess-site")
 (add-hook 'ess-noweb-mode-hook 'rknit-compile-and-print-pdflatex)
 (add-hook 'ess-noweb-font-lock-mode-hook 'rknit-compile-and-print-pdflatex)
 (add-hook 'ess-mode-hook
@@ -16,10 +24,3 @@
 	    (setq ess-use-eldoc nil)))
 
 
-
-(add-hook 'inferior-ess-mode-hook
-	  (lambda ()
-	    (setq eldoc-mode nil)
-	    (setq ess-use-eldoc nil)))
-
-(add-hook 'sh-mode-hook 'highlight-indent-guides-mode)
